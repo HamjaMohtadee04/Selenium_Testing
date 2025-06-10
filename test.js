5cf970644172727182e4dbefaa55998bf4087a5b
HamjaMohtadee <48330979+whitehacker969@users.noreply.github.com>
2025-06-10 06:50:12 +0600

    testing and finding element with xpath code added

--- test.js
+++ test.js
@@ -1,20 +1,34 @@
-// const { Builder, Browser } = require("selenium-webdriver")
+import { Builder, Browser,By}from "selenium-webdriver"
 //click, sendkeys(),scrollview(),select()
+import { expect } from "chai";
+//sync,asyn
+// import { Builder, Browser ,By, Key, Actions} from "selenium-webdriver";
+ let driver = new Builder().forBrowser(Browser.CHROME).build();
+const searchText = "Nike Air"
+const expectedProduct = "Nike air presto by you"
 
-//sync,async
-import { Builder, Browser ,By, Key} from "selenium-webdriver";
-let driver = new Builder().forBrowser(Browser.CHROME).build();
+
 async function testRunner() {
     await driver.manage().window().maximize();
     await driver.get("https://demo.evershop.io/");
+    const siteTitle =await driver.getTitle()
+    const expectedTitle = "An Amazing Evershop Store"
+    expect(expectedTitle).to.eql(siteTitle)
     //get title of website
-    console.log(`Site title: ${await driver.getTitle()}`);
+    // console.log(`Site title: ${await driver.getTitle()}`);
     //find search button
-    await driver.findElement(By.className("search-icon")).click()
+    // await driver.findElement(By.className("search-icon")).click()
 
-    //enter product name on search button
+    //enter product name on search button 
     await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Nike Air",Key.ENTER)
-    // await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys(Key.ENTER)
+    await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys(Key.ENTER)
+   
+    // const actions =driver.actions({async:true})
+    // let element =await driver.findElement(By.xpath("//a[contains(text(),'Shop')]"))
+    // await actions.move({origin:element}).perform()
+
+    // await driver.sleep(5000)
+    // await driver.findElement(By.xpath("//a[contains(text(),'Men')]")).click()
     await driver.sleep(5000);
     await driver.quit();
 }
